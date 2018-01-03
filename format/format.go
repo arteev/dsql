7aa6a41a68751924f18e456357bf81a3c0d1b0d5
Arteev Aleksey <alarteev@yandex.ru>
2018-01-03 20:41:07 +0500

    parsing from flag format for refactor

---
+++ format/format.go
@@ -0,0 +1,178 @@
+package format
+
+import (
+	"errors"
+	"strings"
+	"sync"
+)
+
+var (
+	ErrEmpty = errors.New("Empty format name")
+)
+
+const (
+	groot    = "root"
+	sepGroup = ";"
+	sepName  = ":"
+)
+
+//Format - groups of values for formatting
+type Format struct {
+	sync.RWMutex
+	name   string
+	groups map[string]*Group
+}
+
+//Group - group of values
+type Group struct {
+	sync.RWMutex
+	name string
+	vals map[string]string
+}
+
+//New parsing from string s and returns Format
+func New(s string) (*Format, error) {
+	if strings.TrimSpace(s) == "" {
+		return nil, ErrEmpty
+	}
+	parts := strings.SplitN(s, ":", 2)
+	sgroups := ""
+	if len(parts) > 1 {
+		sgroups = parts[1]
+	}
+	f := &Format{
+		name:   parts[0],
+		groups: make(map[string]*Group),
+	}
+	f.parse(sgroups)
+
+	return f, nil
+}
+
+//Name returns the name of the format
+func (f *Format) Name() string {
+	return f.name
+}
+
+//Count returns the count of groups
+func (f *Format) Count() int {
+	f.RLock()
+	defer f.RUnlock()
+	return len(f.groups)
+}
+
+func (f *Format) parse(s string) error {
+	f.groups[groot] = &Group{name: groot, vals: make(map[string]string)}
+
+	s = strings.TrimSpace(s)
+	if len(s) == 0 {
+		return nil
+	}
+
+	parts := strings.Split(s, sepGroup)
+	for i, p := range parts {
+		gn := strings.SplitN(p, sepName, 2)
+
+		var gname string
+		if len(gn) == 1 && i == 0 {
+			//root
+			//f.groups[groot].parse
+			gname = groot
+		} else {
+			gname = strings.TrimSpace(gn[0])
+		}
+
+		if gname == "" {
+			continue
+		}
+
+		item, ok := f.groups[gname]
+		if !ok {
+			item = &Group{
+				name: gname,
+				vals: make(map[string]string),
+			}
+		}
+		gval := ""
+		if len(gn) > 1 {
+
+			gval = gn[1]
+
+		} else {
+			if gname == groot {
+				gval = gn[0]
+			}
+		}
+
+		item.parse(gval)
+		f.groups[gname] = item
+	}
+
+	return nil
+}
+
+//Root Returns root group
+func (f *Format) Root() *Group {
+	f.RLock()
+	defer f.RUnlock()
+	return f.groups[groot]
+}
+
+//Count returns the count of values
+func (g *Group) Count() int {
+	g.RLock()
+	defer g.RUnlock()
+	return len(g.vals)
+}
+
+func (g *Group) parse(s string) {
+	vals := strings.Split(s, ",")
+	for _, v := range vals {
+		if strings.TrimSpace(v) == "" {
+			continue
+		}
+		kv := strings.SplitN(v, "=", 2)
+		if len(kv) == 1 {
+			g.vals[kv[0]] = ""
+		} else {
+			g.vals[kv[0]] = kv[1]
+		}
+	}
+}
+
+//Get returns a value by key
+func (g *Group) Get(key string) (string, bool) {
+	g.RLock()
+	defer g.RUnlock()
+	val, exist := g.vals[key]
+	if !exist {
+		return "", false
+	}
+	return val, true
+}
+
+//Name returns the name of the group
+func (g *Group) Name() string {
+	return g.name
+}
+
+//Groups returns group names
+func (f *Format) Groups() []string {
+	f.RLock()
+	defer f.RUnlock()
+	groups := make([]string, len(f.groups))
+	i := 0
+	for g := range f.groups {
+		groups[i] = g
+		i++
+	}
+	return groups
+}
+
+//Group returns a group by name
+func (f *Format) Group(name string) (*Group, bool) {
+	f.RLock()
+	defer f.RUnlock()
+	g, ok := f.groups[name]
+	return g, ok
+}
---
+++ format/format_test.go
@@ -0,0 +1,156 @@
+package format
+
+import "testing"
+
+func TestCommon(t *testing.T) {
+	cases := []struct {
+		data      string
+		name      string
+		mustError bool
+		err       error
+	}{
+		{"", "", true, ErrEmpty},
+		{"table", "table", false, nil},
+		{"table:", "table", false, nil},
+		{"table:test", "table", false, nil},
+	}
+
+	for _, c := range cases {
+		f, err := New(c.data)
+		if c.mustError {
+			if err == nil {
+				t.Fatal("Must be error")
+
+			} else if err.Error() != c.err.Error() {
+				t.Errorf("Must be error: %s,got %s", c.err, err)
+			}
+			continue
+		}
+		if !c.mustError && err != nil {
+			t.Error(err)
+			continue
+		}
+
+		if f.Name() != c.name {
+			t.Errorf("Expected name %s,got %s", c.name, f.name)
+		}
+	}
+}
+
+func TestGroups(t *testing.T) {
+	cases := []struct {
+		data   string
+		groups []string
+	}{
+		{"fmttest", []string{groot}},
+		{"fmttest:val=123", []string{groot}},
+		{"fmttest:;;;;;;", []string{groot}},
+		{"fmttest:group1:val=223", []string{groot, "group1"}},
+		{"fmttest:group1:;;;;", []string{groot, "group1"}},
+		{"fmttest:group1:val=223;group2:val2=asd", []string{groot, "group1", "group2"}},
+		{"fmttest:group1:val=223;group1:val2=333", []string{groot, "group1"}},
+		{"fmttest:group1:;group2:;", []string{groot, "group1", "group2"}},
+	}
+	for _, c := range cases {
+		f, err := New(c.data)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if f.Count() != len(c.groups) {
+			t.Errorf("Expected count groups %d, got %d", len(c.groups), f.Count())
+		}
+		for _, g := range c.groups {
+			if _, exists := f.Group(g); !exists {
+				t.Errorf("Expected exist group %q", g)
+			}
+		}
+
+		gotGroups := f.Groups()
+		if len(gotGroups) != len(c.groups) {
+			t.Errorf("Expected Groups() %d, got %d", len(c.groups), len(gotGroups))
+		}
+
+		for _, g := range gotGroups {
+			grGet, exists := f.Group(g)
+			if !exists {
+				t.Errorf("Expected exist group %q", g)
+			}
+			if grGet.Name() != g {
+				t.Errorf("Expected group name %q,got %q", grGet.Name(), g)
+			}
+
+		}
+
+	}
+}
+
+func TestRootValues(t *testing.T) {
+	f, err := New("test:key=val")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, _ := f.Group(groot); f.Root() != got {
+		t.Errorf("Expected group:%q  %v,got %v", groot, f.Root(), got)
+	}
+}
+
+func TestExistValues(t *testing.T) {
+	f, err := New("test:key=val;group1:key2=val2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, exist := f.Root().Get("key2"); exist {
+		t.Errorf("Expected not exists key %q", "key2")
+	}
+
+	gr, _ := f.Group("group1")
+	if _, exist := gr.Get("key33"); exist {
+		t.Errorf("Expected not exists key %q", "key33")
+	}
+}
+func TestGroupsValues(t *testing.T) {
+	cases := []struct {
+		data       string
+		values     map[string]string
+		checkGroup string
+	}{
+		{"test", map[string]string{}, groot},
+		{"test:", map[string]string{}, groot},
+		{"test:key", map[string]string{"key": ""}, groot},
+		{"test:key=val", map[string]string{"key": "val"}, groot},
+		{"test:key=val,key2=val2", map[string]string{"key": "val", "key2": "val2"}, groot},
+		{"test:key=val,", map[string]string{"key": "val"}, groot},
+		{"test:key=val,,,,,,", map[string]string{"key": "val"}, groot},
+		{"test:key=val;group1:key1=val1", map[string]string{"key": "val"}, groot},
+		{"test:group1:key1=val1", map[string]string{"key1": "val1"}, "group1"},
+		{"test:k=v,a=b;group1:key1=val1", map[string]string{"key1": "val1"}, "group1"},
+		{"test:k=v,a=b;group1:key1=val1;group2:x=y,z=k;group3:g=h", map[string]string{"x": "y", "z": "k"}, "group2"},
+	}
+	for _, c := range cases {
+		f, err := New(c.data)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		group, exist := f.Group(c.checkGroup)
+		if !exist {
+			t.Fatalf("Expected group %s,but not exists", c.checkGroup)
+		}
+
+		if group.Count() != len(c.values) {
+			t.Errorf("Expected %d,got %d", len(c.values), group.Count())
+		}
+
+		for k, v := range c.values {
+			got, exist := group.Get(k)
+			if !exist {
+				t.Errorf("Expected exists the key %q", k)
+			} else if got != v {
+				t.Errorf("Expected value:%q,got %q", v, got)
+			}
+		}
+	}
+
+}
+
+//todo: test get key not exists
